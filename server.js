35b1483800ace16e207ce8282de7088cfbc03fb1
Anthony Salemo <ajssalemo@gmail.com>
2021-03-03 10:12:44 -0500

    merged main into master

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const strapi = require("./node_modules/strapi/lib/index");
 const strapiDev = require("./node_modules/strapi/lib/commands/develop");
 
-// DEV_MODE can be set locally to avoid running in production when developing //
+// DEV_MODE can be set locally to avoid running in production when developing 
 if (process.env.DEV_MODE === true) {
   strapiDev();
 } else {
