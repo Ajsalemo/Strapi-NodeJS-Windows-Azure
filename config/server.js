654bbdd795a8a793ccbbe154fec1699562207073
Anthony Salemo <ajssalemo@gmail.com>
2021-03-03 10:57:59 -0500

    fixed host issue

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: "0.0.0.0", 
+  host: "<your-azure-site-name>.azurewebsites.net", 
   // When deployed, listen to the PORT variable - else when running locally, listen to 1337
   port: process.env.PORT || '1337',
   admin: {
     auth: {
+      // This can be changed to be environment variables 
       secret: env("ADMIN_JWT_SECRET", "0d1286debf5e50237e4b6861aa359d5d"),
     },
   },
